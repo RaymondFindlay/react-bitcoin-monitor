02b1bf4a92fc1d342d0b73a8d1cd553b95540cbf
Raymond Findlay <raymond@automated-intelligence.com>
2020-07-06 16:18:18 +0100

    Updates

---
+++ precache-manifest.f247f5dfd814f1f6e2ecb523b8f56e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1195f0a2faac66050f342199cc7136db",
+    "url": "/react-bitcoin-monitor/index.html"
+  },
+  {
+    "revision": "3c6703fa56f4aef1ad0e",
+    "url": "/react-bitcoin-monitor/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f24f49189d5f91069969",
+    "url": "/react-bitcoin-monitor/static/js/2.d19b13ff.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-bitcoin-monitor/static/js/2.d19b13ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6703fa56f4aef1ad0e",
+    "url": "/react-bitcoin-monitor/static/js/main.4b4104b0.chunk.js"
+  },
+  {
+    "revision": "c641328b61eac6c4499f",
+    "url": "/react-bitcoin-monitor/static/js/runtime-main.97b71bfe.js"
+  }
+]);
